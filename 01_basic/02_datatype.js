70c11ced80416a99148d3deb0fa0492c0e4d0060
pavan <pavan@gmail.com>
2026-04-19 01:24:40 +0530

    gg

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,41 @@
+//alert(3+4) ->no use the js ,its use the browser(inspect)
+//number =>2 to power 53
+//bigits
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefine=>
+//symbole=>unique
+
+console.log(typeof underdefine);//output->underfine
+console.log(typeof null);//output->object
+let score=true
+
+console.log(typeof(score))
+//"33abc"=NaN
+// tree=>1
+//false=>0
+//true=>1
+//" "=>0
+//"abc"=>NaN
+//null=>0
+//undefined=>NaN    
+let someNumber=33
+let variableNumber=Number(someNumber)
+console.log(variableNumber)
+console.log(typeof variableNumber)//thaTs mean we can convert the string to number by using Number() function
+
+
+//************OPERATION******************
+// console.log(3+4)//output->7
+// console.log(3-4)//output->-1
+// console.log(3*4)//output->12
+// console.log(3/4)//output->0.75
+// console.log(3%4)//output->3
+// console.log(3**4)//output->81
+  
+
+//complex sichuation or code conversion
+console.log("1"+3)
+console.log("1"-3)
+console.log("2"+2+3)
---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2<3)
+// console.log(2>3)
+// console.log(2<=3)
+// console.log(2>=3)
+// console.log(2==3)
+// console.log(2!=3)
+
+
+console.log("3">4)
+console.log("3"<4)
+
+
+console.log(null>0)
+console.log(null==0)
+
+
+//stricly Checking(===) the value and data type
+
+console.log(2===2)
+console.log(2==="2")
+console.log("2"===2)
