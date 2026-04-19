943584a98e07a63af2702cf3c217582e966f4516
pavan <pavan@gmail.com>
2026-04-19 11:07:16 +0530

    jjj

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,7 @@
+const accountId="344"
+var accountPassword="123"
+let accountEmail="vijay@gmai.com"
+accountcity="karnataka"
+console.log()
+//accountId=2123->undefine
+console.table([accountId,accountEmail,accountEmail,accountcity])
